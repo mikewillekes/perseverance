86baf7cd95ce2e884ce5966a1c329df64791eb31
Mike Willekes <mikewillekes@gmail.com>
2023-07-03 10:53:03 -0400

    Simple vector search using Docarray

--- src/run_pipeline_step_6_vector_search.py
+++ src/run_pipeline_step_6_vector_search.py
@@ -1,21 +1,36 @@
 from config import config
 from dossier import load_gpt_cleaned_diagnoses
 from icd import load_icd_entities_generator
+import numpy as np
 
-from collections import defaultdict
+from docarray import DocList
+from docarray.documents import TextDoc
+from docarray.typing import NdArray
+from docarray.index import InMemoryExactNNIndex
 
-# First - Collect embeddings for Field Diagnoses
-unique_gpt_cleaned_diagnoses = []
+
+class MyDoc(TextDoc):
+    embedding: NdArray[1536]
+
+db = InMemoryExactNNIndex[MyDoc]()
+docs = DocList[MyDoc]()
+
+
+for entity in load_icd_entities_generator(config.ICD_ENTITIES_FILE_EMBEDDING):
+    docs.append(MyDoc(text=f'{entity.code}: {entity.title}', embedding=np.array(entity.embeddings)))
+db.index(docs)
+
 
 for diagnosis in load_gpt_cleaned_diagnoses(config.GPT_CLEANED_DIAGNOSES_FILE):
     if diagnosis.clean_field_diagnosis_french and diagnosis.embeddings:
-        unique_gpt_cleaned_diagnoses.append(diagnosis.clean_field_diagnosis_french)
+        print('============================')
+        query = MyDoc(text=diagnosis.clean_field_diagnosis_english, embedding=np.array(diagnosis.embeddings))
+        print(f'QUERY: {diagnosis.clean_field_diagnosis_english}')
+        
+        MAX_RESULTS = 10
 
+        matches, scores = db.find(query, search_field='embedding', limit=MAX_RESULTS)
 
-unique_gpt_cleaned_diagnoses = list(set(unique_gpt_cleaned_diagnoses))
-unique_gpt_cleaned_diagnoses.sort()
+        for i in range(0, len(matches)):
+            print(f'\t{matches[i].text} : {scores[i]}')
 
-for d in unique_gpt_cleaned_diagnoses:
-    print(d)
-
-print(f'Unique Diagnoses: {len(unique_gpt_cleaned_diagnoses)}')
