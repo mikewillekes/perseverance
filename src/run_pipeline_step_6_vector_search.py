c08977407ae0fba5b46d1fa94ca3d6648b50aaae
Mike Willekes <mikewillekes@gmail.com>
2023-07-29 14:28:04 -0400

    ANN Search

--- src/run_pipeline_step_6_vector_search.py
+++ src/run_pipeline_step_6_vector_search.py
@@ -17,9 +17,19 @@ docs = DocList[MyDoc]()
 
 
 for entity in load_icd_entities_generator(config.ICD_ENTITIES_FILE_EMBEDDING):
+    # Because of the rough field notes, just consider "top level" items like
+    #   "code":"1C1D","title":"Yaws"
+    # not
+    #   "code":"1C1D.0","title":"Primary yaws"
+    #   "code":"1C1D.1","title":"Secondary yaws"
+    #   "code":"1C1D.2","title":"Tertiary yaws"
+    #   "code":"1C1D.3","title":"Latent yaws"
+    #
+    #if '.' not in entity.code:
     docs.append(MyDoc(text=f'{entity.code}: {entity.title}', embedding=np.array(entity.embeddings)))
 db.index(docs)
 
+all_diagnoses = []
 
 for diagnosis in load_gpt_cleaned_diagnoses(config.GPT_CLEANED_DIAGNOSES_FILE):
     if diagnosis.clean_field_diagnosis_french and diagnosis.embeddings:
@@ -27,10 +37,19 @@ for diagnosis in load_gpt_cleaned_diagnoses(config.GPT_CLEANED_DIAGNOSES_FILE):
         query = MyDoc(text=diagnosis.clean_field_diagnosis_english, embedding=np.array(diagnosis.embeddings))
         print(f'QUERY: {diagnosis.clean_field_diagnosis_english}')
         
-        MAX_RESULTS = 10
+        MAX_RESULTS = 50
 
         matches, scores = db.find(query, search_field='embedding', limit=MAX_RESULTS)
 
         for i in range(0, len(matches)):
-            print(f'\t{matches[i].text} : {scores[i]}')
+            if scores[i] > 0.8:
+                print(f'\t{matches[i].text} : {scores[i]}')
 
+        all_diagnoses.append(diagnosis.clean_field_diagnosis_english)
+
+unique_diagnoses = list(set(all_diagnoses))
+unique_diagnoses.sort()
+
+for d in unique_diagnoses:
+    print(d)
+print(len(unique_diagnoses))
---
+++ src/run_pipeline_step_7_summary_data.py
@@ -0,0 +1,29 @@
+from config import config
+from dossier import load_gpt_cleaned_diagnoses
+from icd import load_icd_entities_generator
+import numpy as np
+
+
+print("\n\nIs Probable")
+print('============================')
+for diagnosis in load_gpt_cleaned_diagnoses(config.GPT_CLEANED_DIAGNOSES_FILE):
+    if diagnosis.is_probable and diagnosis.clean_field_diagnosis_french and diagnosis.embeddings:
+        print(f'{diagnosis.raw_field_diagnosis_french} > {diagnosis.clean_field_diagnosis_french}')
+
+print("\n\nTo Eliminate")
+print('============================')
+for diagnosis in load_gpt_cleaned_diagnoses(config.GPT_CLEANED_DIAGNOSES_FILE):
+    if diagnosis.to_eliminate and diagnosis.clean_field_diagnosis_french and diagnosis.embeddings:
+        print(f'{diagnosis.raw_field_diagnosis_french} > {diagnosis.clean_field_diagnosis_french}')
+
+print("\n\nTo Investigate")
+print('============================')
+for diagnosis in load_gpt_cleaned_diagnoses(config.GPT_CLEANED_DIAGNOSES_FILE):
+    if diagnosis.to_investigate and diagnosis.clean_field_diagnosis_french and diagnosis.embeddings:
+        print(f'{diagnosis.raw_field_diagnosis_french} > {diagnosis.clean_field_diagnosis_french}')
+
+print("\n\nShorthand")
+print('============================')
+for diagnosis in load_gpt_cleaned_diagnoses(config.GPT_CLEANED_DIAGNOSES_FILE):
+    if diagnosis.shorthand and diagnosis.clean_field_diagnosis_french and diagnosis.embeddings:
+        print(f'[{diagnosis.shorthand}] {diagnosis.raw_field_diagnosis_french} > {diagnosis.clean_field_diagnosis_french}')
